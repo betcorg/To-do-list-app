23430888fe986952f66bd3f80cb72ba9ef46f71e
H. Franc <betcorg@gmail.com>
2022-05-05 13:05:34 -0500

    Las tareas completas se pasan a un contenedor separado

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-
+const container = document.querySelector('.container');
 const btnNewTask = document.querySelector('#push');
 const tasks = document.querySelector('.tasks');
-const msgEmpty = document.querySelector('.tasks p')
+const msgEmpty = document.querySelector('.tasks p');
 
 btnNewTask.addEventListener('click', addTask);
 
 
 // Crea el HTML de la tarea y lo agrega al panel de tareas pendientes
-function addTask() {
+function addTask(s) {
+
     const taskName = document.querySelector('.newtask input').value
     if (taskName.length === 0) {
-
         alert('Agrega una tarea');
-
     } else {
-
         const newTask = document.createElement('div');
-
         newTask.classList.add('task');
         newTask.innerHTML = `
             <input type="checkbox"></input>
@@ -27,37 +24,61 @@ function addTask() {
                 <i class="fa-regular fa-trash-can"></i>
             </button>
         `;
-
         tasks.appendChild(newTask);
         msgEmpty.style.display = 'none';
-        document.querySelector('.newtask input').value = '';
-
-        // Agrega event listener al boton borrar de las nuevas tareas.
-        let delTask = document.querySelectorAll('.delete');
-        for (let i = 0; i < delTask.length; i++) {
-            delTask[i].addEventListener('click', deleteTask);
-        }
-
-        // Marca como comletadas las tareas selecionadas
-        let currenttask = document.querySelectorAll('.task input');
-        for (let i = 0; i < currenttask.length; i++) {
-            currenttask[i].addEventListener('click', completeTask);
-        }
+        document.querySelector('.newtask input').value = ''
+        checkBoxActive();
+        btnDeleteActive();
     }
 }
 
-function deleteTask() {
-    this.parentNode.remove();
-    if (document.querySelectorAll('.delete').length === 0) {
-        msgEmpty.style.display = 'block';
-        msgEmpty.style.color = 'green';
-        msgEmpty.innerText = '¡Hurra! Ya no tienes tareas pendientes';
+
+// Activa el listener del checkbox para marcar completadas las tareas
+function checkBoxActive() {
+    let currentTask = document.querySelectorAll('.task input');
+    for (let i = 0; i < currentTask.length; i++) {
+        currentTask[i].addEventListener('click', completeTask);
     }
 }
 
+ // Activa el botón borrar en las nuevas tareas
+ function btnDeleteActive() {
+    let delTask = document.querySelectorAll('.delete');
+    for (let i = 0; i < delTask.length; i++) {
+        delTask[i].addEventListener('click', deleteTask);
+    }
+    function deleteTask() {
+        this.parentNode.remove();
+        msg();
+    }
+}
+
+// Cuando se selecciona el checbox se dispara este código para completar la tarea y mandarla al contenedor de tareas completas
 function completeTask() {
-    this.parentNode.classList.toggle('completed');
+    const doneTaskContainer = document.querySelector('#done-container');
+    const newDoneTask = document.createElement('div');
+    newDoneTask.classList.add('task');
+    newDoneTask.innerHTML = this.parentNode.innerHTML;
+    newDoneTask.classList.toggle('completed');
+    doneTaskContainer.appendChild(newDoneTask);
+    this.parentNode.remove();
+    msg();
+    btnDeleteActive()
 }
+
+
+// Despliega un mensaje cuando ya no hay tareas pendientes
+function msg() {
+    if (document.querySelectorAll('#current .delete').length === 0) {
+        msgEmpty.innerText = '¡Hurra! Ya no tienes tareas pendientes';
+        msgEmpty.style.color = 'green';
+        msgEmpty.style.display = 'block';
+    }
+}
+
+
+
+
 
 
 
